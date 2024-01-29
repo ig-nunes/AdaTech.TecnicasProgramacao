8d7cc4cb0e1d35f301dd5c3be03ce89f7f37083b
Igor Nunes <inunes.us@gmail.com>
2024-01-29 09:22:06 -0300

    feat: add exercicio1

---
+++ OperacoesLista/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio1.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void FiltroLista10ouMais(this List<string> lista)
+        {
+            var listaAux = new List<string>();
+            foreach (var item in lista)
+            {
+                if (item.Length >= 10)
+                {
+                    listaAux.Add(item);
+                }
+            }
+
+            lista.Clear();
+            lista.AddRange(listaAux);
+        }
+    }
+}
---
+++ OperacoesLista/OperacaoListas.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio1
+{
+    public static class OperacaoListas
+    {
+        public static void FiltroLista10ouMais(ref List<string> lista)
+        {
+            var listaAux = new List<string>();
+            foreach (var item in lista)
+            {
+                if (item.Length >= 10)
+                {
+                    listaAux.Add(item);
+                }
+            }
+
+            lista = listaAux;
+        }
+
+    }
+}
