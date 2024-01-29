d1eca43dcecaf6993eb531716cb9ad6f9f1163d0
Igor Nunes <inunes.us@gmail.com>
2024-01-29 09:22:51 -0300

    feat: add exercicio4

---
+++ Exercicio4/OperacaoDicionario.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio4
+{
+    public class OperacaoDicionario
+    {
+        private Dictionary<string, int> _contador;
+
+        public void ContarPalavras(string texto)
+        {
+            Dictionary<string, int> contador = new Dictionary<string, int>();
+
+            string[] palavras = texto.Split(new char[] { ' ', '.', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string palavra in palavras)
+            {
+                string palavraFormatada = palavra.ToLower();
+                if (contador.ContainsKey(palavraFormatada))
+                {
+                    contador[palavraFormatada]++;
+                }
+                else
+                {
+                    contador[palavraFormatada] = 1;
+                }
+            }
+
+            _contador = contador;
+        }
+
+        public void ImprimirContador()
+        {
+            Console.WriteLine("Contagem de palavras:");
+
+            foreach (var entrada in _contador)
+            {
+                Console.WriteLine($"{entrada.Key}: {entrada.Value}");
+            }
+        }
+    }
+}
